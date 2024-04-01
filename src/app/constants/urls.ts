c0a8380f682c72bdd285eda8fed0282125f7011d
wolfenmor <wolfenmor@gmail.com>
2024-04-01 20:33:26 +0200

    lesson2

---
+++ src/app/constants/urls.ts
@@ -0,0 +1,17 @@
+const baseURL = "https://jsonplaceholder.typicode.com"
+const users = `${baseURL}/users`
+const posts = `${baseURL}/posts`
+
+const urls = {
+  users: {
+    base: users,
+    byId:(id: number):string => `${users}/${id}`
+  },
+  posts: {
+    base: posts
+  }
+}
+
+export {
+  urls
+}
